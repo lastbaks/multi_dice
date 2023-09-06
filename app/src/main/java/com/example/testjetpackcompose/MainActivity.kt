93c81ad09f6bf2857fcdf0d357aaefae2be465ac
lastbaks <lastbaks@gmail.com>
2023-09-06 07:36:49 +0300

    выведение randomDice в функцию.

--- app/src/main/java/com/example/testjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/testjetpackcompose/MainActivity.kt
@@ -14,17 +14,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.testjetpackcompose.ui.theme.TestJetpackComposeTheme
 
@@ -42,7 +43,7 @@ class MainActivity : ComponentActivity() {
                     for(i in 0 .. 5){
                         dicies.add(random())
                     }
-                    testCompose(dicies)
+                    bodyView(dicies)
                     }
                 }
             }
@@ -51,7 +52,7 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun testCompose(dicies: MutableList<Int>) {
+fun bodyView(dicies: MutableList<Int>) {
 
     Row(modifier = Modifier.fillMaxSize()){
         Column(modifier = Modifier
@@ -60,7 +61,8 @@ fun testCompose(dicies: MutableList<Int>) {
             Box(modifier = Modifier
                 .fillMaxSize()
                 .padding(15.dp)
-                .background(Color.Blue)){
+                .background(Color.Blue),
+                contentAlignment = Alignment.Center){
                 Button(onClick = {
                     for(i in 0 .. 5){
                         dicies[i] = (random())
@@ -85,9 +87,10 @@ fun testCompose(dicies: MutableList<Int>) {
                     .padding(15.dp)
                     .height(120.dp)
                     .fillMaxWidth()
-                    .background(colorResource(R.color.dice_background))){
+                    .background(colorResource(R.color.dice_background)),
+                    contentAlignment = Alignment.Center){
                     Image(
-                        painter = painterResource(id = randomDice), contentDescription = "1"
+                        painter = painterResource(id = randomDice(1)), contentDescription = "1"
                     )
                 }
             }
@@ -98,4 +101,15 @@ fun testCompose(dicies: MutableList<Int>) {
 fun random(): Int {
     val localRand = (1 .. 6).random()
     return localRand
+}
+
+fun randomDice(i: Int) : Int {
+    val randomDice = when (i) {
+        1 -> return R.drawable.dice_1
+        2 -> return R.drawable.dice_2
+        3 -> return R.drawable.dice_3
+        4 -> return R.drawable.dice_4
+        5 -> return R.drawable.dice_5
+        else -> return R.drawable.dice_6
+    }
 }
